141339122156d178ab79719d63fe155a41fcaebb
Mark Jenquine <markjenquine@MacBook-Pro-2.local>
2020-01-06 12:32:52 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,18 +1,20 @@
 let cards = ["queen", "queen", "king", "king"];
 let cardsInPlay = [];
-let cardOne = cards[0];
-let cardTwo = cards[2];
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
 
-
-if (cardsInPlay.length === 2) {
- if (cardsInPlay[0] === cardsInPlay[1]) {
-	alert("You found a match!");
-} else if (cardsInPlay[0] !== cardsInPlay[1]) {
-	alert("Sorry, try again");
+function checkForMatch() {
+	if (cardsInPlay[0] === cardsInPlay[2]) {
+		console.log("You found a match!");
+		} else {
+		console.log("Sorry, try again.");
+}
+}
+function flipCard(cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	if (cardsInPlay.length === 2) {
+	checkForMatch();	
 }
 }
 
+flipCard(0);
+flipCard(2);
